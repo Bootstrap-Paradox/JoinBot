047046b32d707368554e55ddc7a5369240ca99b7
Sid Kuchnure <sidkuchnure@gmail.com>
2022-01-14 19:24:19 +0530

    Update Bot
    - Add new chrome Driver

--- main.py
+++ main.py
@@ -3,10 +3,28 @@ from selenium.webdriver.chrome.options import Options
 
 import time
 
+
 class Bot:
 
-    pass
+    def __init__(self,) -> None:
+        self.options = Options()
+        self.options.add_argument(
+            '--disable-blink-features=AutomationControlled')
+        self.options.add_argument('--start-maximized')
+        self.options.add_experimental_option("prefs", {
+            "profile.default_content_setting_values.media_stream_mic": 1,
+            "profile.default_content_setting_values.media_stream_camera": 1,
+            "profile.default_content_setting_values.geolocation": 0,
+            "profile.default_content_setting_values.notifications": 1
+        })
+
+        self.driver = webdriver.Chrome(options=self.options)
+
+    def join_meeting(self, meeting_link):
+        self.driver.get(meeting_link)
 
 
 if __name__ == "__main__":
-    pass
+
+    bot = Bot()
+    bot.join_meeting(meeting_link="https://meet.google.com/wwn-vpwo-jvc")
